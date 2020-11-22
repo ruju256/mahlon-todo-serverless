dc40464447ee9382c662446a7c09d00dd3a9d425
Mahlon <ezramahlon@gmail.com>
2020-11-22 17:26:28 +0300

    added Auth0 credentials

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-zy6cf--2.us.auth0.com',            // Auth0 domain
+  clientId: 'UWD6MtMtXrShDgJ6zOuUneywfqXidKxs',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
