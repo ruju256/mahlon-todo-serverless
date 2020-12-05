7ce630fbaf4b92a8b9438a170ba4669c1e19169e
Mahlon <ezramahlon@gmail.com>
2020-12-05 14:55:10 +0300

    validating incoming requests

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -24,7 +24,7 @@ export const handler = middy(
           'Access-Control-Allow-Credentials': true
         },
         body: JSON.stringify({
-          newItem
+          item: newItem
         })
       }
     } catch (e) {
